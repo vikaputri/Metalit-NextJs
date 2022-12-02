88292d2b6d864906efb69574247300abee73d3ea
vikaputri <vikaputri909@gmail.com>
2022-12-03 06:49:10 +0700

    vercel

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Dashboard = () => {
   return (
@@ -9,9 +10,11 @@ const Dashboard = () => {
                 <div className="row flex-nowrap">
                     <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 biru">
                         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                            <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                                <span className="fs-5 d-none d-sm-inline">Metalit</span>
-                            </a>
+                            <Link href="/">
+                                <a className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                                    <span className="fs-5 d-none d-sm-inline">Metalit</span>
+                                </a>
+                            </Link>
                             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                                 <li className="nav-item">
                                     <a href="#" className="nav-a align-middle px-0 text-white">
