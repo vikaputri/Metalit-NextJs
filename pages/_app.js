97d655fd9ae16ded560bebee9f7e0bb7758e1b7b
vikaputri <vikaputri909@gmail.com>
2022-12-03 07:10:35 +0700

    vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 
 import '../styles/globals.css'
 import Head from "next/head";
-import Script from 'next/Script';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
